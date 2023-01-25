b84034e38557516c7eea9d687478fc943857c808
Maxwell <maxpasqualis@gmail.com>
2023-01-24 18:09:38 -0800

    initialized tilemap rendering (currently a temp map)

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,26 +1,41 @@
 import React, { Component } from "react";
 import Phaser from "phaser";
 
+const WIDTH = 800;
+const HEIGHT = 600;
+
 class Game extends Component {
   componentDidMount() {
-    var config = {
+    const config = {
       type: Phaser.AUTO,
       parent: "game-container",
       backgroundColor: "#EBEBEB",
-      width: 800,
-      height: 600,
+      width: WIDTH,
+      height: HEIGHT,
+      pixelArt: true,
       scene: {
-        create() {
-          this.add
-            .text(150, 60, "GAME")
-            .setFontFamily("Arial")
-            .setFontSize(80)
-            .setOrigin(0.5)
-            .setColor("#000000");
-        },
+        preload: preload,
+        create: create,
+        // update: update,
       },
     };
     this.game = new Phaser.Game(config);
+
+    function preload() {
+      this.load.image("tiles", "assets/tilemaps/debug-tiles.png");
+      this.load.tilemapTiledJSON("map", "assets/tilemaps/debug-map.json");
+    }
+
+    function create() {
+      const map = this.make.tilemap({ key: "map" });
+      const tiles = map.addTilesetImage("debug-tiles", "tiles");
+      const bg = map.createLayer("background", tiles, 0, 0);
+      const walls = map.createLayer("walls", tiles, 0, 0);
+
+      this.cameras.main.setBounds(0, 0, bg.width, bg.height, true).setZoom(5);
+    }
+
+    // function update(time, delta){}
   }
   render() {
     return <div id="game-container"></div>;
