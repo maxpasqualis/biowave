8c1cc1b69b6296fe2244441ecf6724b06a0d243c
Maxwell <maxpasqualis@gmail.com>
2023-02-01 16:16:03 -0800

    created textbox scene

---
+++ src/biowave/textbox.js
@@ -0,0 +1,13 @@
+import Phaser from "phaser";
+
+class Textbox extends Phaser.Scene {
+  constructor() {
+    super("Textbox");
+  }
+
+  preload() {}
+  create() {}
+  update() {}
+}
+
+export default Textbox;
