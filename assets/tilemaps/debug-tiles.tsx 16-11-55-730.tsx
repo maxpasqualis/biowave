58230051081f31e2b6eda2b9a7ce76b466763632
Maxwell <maxpasqualis@gmail.com>
2023-02-06 18:06:48 -0800

    deploy to github pages

---
+++ assets/tilemaps/debug-tiles.tsx 16-11-55-730.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="debug-tiles" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="debug-tiles.png" width="128" height="128"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
